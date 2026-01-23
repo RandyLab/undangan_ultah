421fa02d1a8d6b2b8ce25a46e92192c873373275
RandyLab <nuggetnyafiesta45@gmail.com>
2026-01-23 07:47:50 +0700

    Commit untuk Deploy

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/undangan_ultah /",
-  plugins: [
-    tailwindcss(),
-  ],
-})
+	base: "/undangan_ultah/",
+	plugins: [tailwindcss()]
+});
